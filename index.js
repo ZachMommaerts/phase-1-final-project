9aec4e7336ed13ef03c04155296f6f3cbd96f72d
John DeFeo <jackd3243@gmail.com>
2021-11-16 17:35:54 -0500

    Review submit and toggle function

--- index.js
+++ index.js
@@ -9,46 +9,47 @@ const bookReviewForm = document.querySelector('#review-form');
 const bookReview = document.querySelector('#review');
 const likeButton = document.querySelector('#like-button');
 const bookContainer = document.querySelector('.book-details');
+const bookReviewList = document.querySelector("#book-review")
+// //Fetch request to API to get data regarding random book
+// fetch('https://openlibrary.org/works/OL45883W.json')
+// .then(res => res.json())
+// .then(book => renderTitle(book))
+// .catch(error => alert(error))
 
-//Fetch request to API to get data regarding random book
-fetch('https://openlibrary.org/works/OL45883W.json')
-.then(res => res.json())
-.then(book => renderTitle(book))
-.catch(error => alert(error))
+// //Fetch request to API to get book cover
+// fetch(`https://covers.openlibrary.org/b/isbn/${}-L.jpg`)
+// .then(res => res.json())
+// .then(cover => renderCover(cover))
+// .catch(error => alert(error))
 
-//Fetch request to API to get book cover
-fetch(`https://covers.openlibrary.org/b/isbn/${}-L.jpg`)
-.then(res => res.json())
-.then(cover => renderCover(cover))
-.catch(error => alert(error))
+// //Fetch request to API to get author name
+// fetch(`https://openlibrary.org/authors/${}.json`)
+// .then(res => res.json())
+// .then(book => renderBook(book))
+// .catch(error => alert(error))
 
-//Fetch request to API to get author name
-fetch(`https://openlibrary.org/authors/${}.json`)
-.then(res => res.json())
-.then(book => renderBook(book))
-.catch(error => alert(error))
+// //Fetch request to API to get publishing date
 
-//Fetch request to API to get publishing date
+// //Function that renders title on the page
+// function renderTitle(book) {
+//     bookTitle.textContent = book.title;
+// }
 
-//Function that renders title on the page
-function renderTitle(book) {
-    bookTitle.textContent = book.title;
-}
-
-//Function that renders image on the page
-function renderCover(cover) {
-    bookCover.src = 
-}
+// //Function that renders image on the page
+// function renderCover(cover) {
+//     bookCover.src = 
+// }
 
 likeButton.addEventListener("click", (e) =>{
-
+    e.classList.toggle(likeButton)
 })
 
 bookReviewForm.addEventListener("submit", (e) =>{
     e.preventDefault();
     let bookReview = document.querySelector("#review").value;
-    let li =document.createElement("li");
-    li.textContent = bookReview;
-    bookReviewForm.appendChild(li);
+    let li = document.createElement("li");
+    li.className = "input-review"
+    li.textContent = bookReview
+    bookReviewList.appendChild(li)
     e.target.reset()
 })
