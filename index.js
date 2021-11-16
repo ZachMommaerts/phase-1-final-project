67ce77ae228c53cb2153cc2d3e01a0a188aaa291
Zach <zmommaerts@gmail.com>
2021-11-16 09:46:53 -0600

    added a list of variables for use in js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+//List of variables
+const bookTitle = document.querySelector('#book-title')
+const bookCover = document.querySelector('#book-cover')
+const bookAuthor = document.querySelector('#book-author')
+const bookPublisher = document.querySelector('#book-publisher')
+const bookPublishingDate = document.querySelector('#book-publishing-date')
+const bookISBN = document.querySelector('#ISBN')
+const bookReviewForm = document.querySelector('#review-form')
+const bookReview = document.querySelector('#review')
+const likeButton = document.querySelector('#like-button')
+
 //Fetch request to server to get data regarding random book
-// fetch('')
-// .then(res => res.json())
-// .then(book => renderBook(book))
-// .catch(error => alert(error))
+fetch('')
+.then(res => res.json())
+.then(book => renderBook(book))
+.catch(error => alert(error))
 
 //Function that renders book on the page
 function renderBook(book) {
