0ab8fa82d6e37c570021107a9c059abd7789addb
Zach <zmommaerts@gmail.com>
2021-11-16 17:09:29 -0600

    added config file for api key and started using new api

--- index.js
+++ index.js
@@ -10,33 +10,16 @@ const bookReview = document.querySelector('#review');
 const likeButton = document.querySelector('#like-button');
 const bookContainer = document.querySelector('.book-details');
 
+//Gets API key from hidden JS file
+const googleKey = config.GoogleAPIKey;
+
 //Fetch request to API to get data regarding random book
-fetch('https://openlibrary.org/works/OL45883W.json')
+fetch(`https://www.googleapis.com/books/v1/volumes?q=search-terms&${googleKey}`)
 .then(res => res.json())
-.then(book => renderTitle(book))
+.then(book => console.log(book))
 .catch(error => alert(error))
 
-//Fetch request to API to get book cover
-fetch(`https://covers.openlibrary.org/b/isbn/${}-L.jpg`)
-.then(res => res.json())
-.then(cover => renderCover(cover))
-.catch(error => alert(error))
+//Function for rendering book on website
+function renderBook(book) {
 
-//Fetch request to API to get author name
-fetch(`https://openlibrary.org/authors/${}.json`)
-.then(res => res.json())
-.then(book => renderBook(book))
-.catch(error => alert(error))
-
-//Fetch request to API to get publishing date
-
-//Function that renders title on the page
-function renderTitle(book) {
-    bookTitle.textContent = book.title;
 }
-
-//Function that renders image on the page
-function renderCover(cover) {
-    bookCover.src = 
-}
-
